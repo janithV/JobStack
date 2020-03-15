15b0837942bedce2f8236ccba8f9b9a72037c481
medhamunasinghe <medhamunasinghe2000@gmail.com>
2020-03-15 23:08:21 +0530

    created home page and post job page
    generated the post-job component and designed post-job page with home page

--- angular-jobstack/src/app/app-routing.module.ts
+++ angular-jobstack/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PostJobComponent } from './post-job/post-job.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'post-job', component: PostJobComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-jobstack/src/app/app.module.ts
+++ angular-jobstack/src/app/app.module.ts
@@ -3,18 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatIconModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { PostJobComponent } from './post-job/post-job.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule, MatInputModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    PostJobComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
