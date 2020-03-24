13767ecd10bc09ab38270630808a3e1124212786
MahenPD <61178225+MahenPD@users.noreply.github.com>
2020-03-24 14:11:37 +0530

    node files(Login Registration)

---
+++ nodejs-jobstack/config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+'connection':{
+    'host':'remotemysql.com',
+    'user':'RtG8BK6Pbe',
+    'password':'bXH6YfYQMr'
+   },
+   'database':'RtG8BK6Pbe',
+   'user_table':'UserTable'
+  };
---
+++ nodejs-jobstack/config/passport.js
@@ -0,0 +1,172 @@
+var LocalStrategy = require("passport-local").Strategy;
+
+var mysql = require('mysql');
+var bcrypt = require('bcrypt-nodejs');
+var dbconfig = require('./database');
+var connection = mysql.createConnection(dbconfig.connection);
+
+connection.query('USE ' + dbconfig.database);
+
+module.exports = function(passport) {
+ passport.serializeUser(function(user, done){
+  done(null, user.id);
+ });
+
+ passport.deserializeUser(function(id, done){
+  connection.query("SELECT * FROM UserTable WHERE userId = ? ", [id],
+   function(err, rows){
+    done(err, rows[0]);
+   });
+ });
+
+ passport.use(
+  'local-signup',
+  new LocalStrategy({
+   usernameField : 'username',
+   passwordField: 'password',
+   passReqToCallback: true
+  },
+  function(req, username, password, done){                                                      
+      if (req.body.codingskills) {
+        var codeskill = 1;
+      }
+      else{
+        codeskill = 0;
+    }
+    if (req.body.socialskills) {
+      var socialskill = 1;
+    }
+    else{
+      socialskill = 0;  
+  }
+  if (req.body.webskills) {
+    var webskill = 1;
+  }
+  else{
+    webskill = 0;  
+}
+if (req.body.languageskills) {
+  var langskill = 1;
+}
+else{
+  langskill = 0;  
+}
+if (req.body.programeskills) {
+  var programeskill = 1;
+}
+else{
+  programeskill = 0;  
+}
+
+if (req.body.Specialization1) {
+  var backend = 1;
+}
+else{
+  backend = 0;  
+}
+if (req.body.Specialization2) {
+  var frontend = 1;
+}
+else{
+  frontend = 0;  
+}
+if (req.body.Specialization3) {
+  var fullstack = 1;
+}
+else{
+  fullstack = 0;  
+}
+if (req.body.Specialization4) {
+  var mobile = 1;
+}
+else{
+  mobile = 0;  
+}
+if (req.body.Specialization5) {
+  var web = 1;
+}
+else{
+  web = 0;  
+}
+if (req.body.Specialization6) {
+  var uiux = 1;
+}
+else{
+  uiux = 0;  
+}
+
+   connection.query("SELECT * FROM UserTable WHERE userEmail = ? ",                                  
+   [username], function(err, rows){
+    if(err)
+     return done(err);
+    if(rows.length){
+     return done(null, false, req.flash('signupMessage', 'That is already taken'));
+    }else{
+    var count = 1;
+     var newUserMysql = {
+      count: count, 
+      firstname: req.body.fname,
+      lastname: req.body.lname,
+      birthdate: req.body.dob,
+      gender: req.body.gender,
+      username: username,
+      password: bcrypt.hashSync(password, null, null),
+      university: req.body.university,
+      school: req.body.school,
+      qualification: req.body.degree ,
+      codskill: codeskill,
+      socskill: socialskill,
+      webskill: webskill,
+      langskill: langskill,
+      programeskill: programeskill,
+      backend: backend,
+      frontend: frontend,
+      fullstack: fullstack,
+      web: web,
+      mobile: mobile,
+      uiux: uiux
+     };
+     console.log(newUserMysql);
+
+     var insertQuery = "INSERT INTO UserTable (userId, userFirstName, userLastName, education, dateOfBirth, gender,	userEmail,	userPassword, codingSkill, socialSkill, languageSkill, programDevelopment, degreeId, frontEndDevelopment, backEndDevelopment, fullStack, mobileDevelopment, webDevelpoment, uiUx) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,? )";
+
+     connection.query(insertQuery, [newUserMysql.count,newUserMysql.firstname, newUserMysql.lastname, newUserMysql.university, newUserMysql.birthdate, newUserMysql.gender, newUserMysql.username, newUserMysql.password,newUserMysql.codskill,newUserMysql.socskill,newUserMysql.langskill,newUserMysql.programeskill,newUserMysql.qualification, newUserMysql.frontend, newUserMysql.backend, newUserMysql.fullstack, newUserMysql.mobile, newUserMysql.web, newUserMysql.uiux],
+      function(err, rows){
+        if(err){
+          console.log(err);
+        }
+        else{
+       newUserMysql.id = rows.insertId;
+       return done(null, newUserMysql);
+        }
+      });
+    }
+   });
+  })
+ );
+
+ passport.use(
+  'local-login',
+  new LocalStrategy({
+   usernameField : 'username',
+   passwordField: 'password',
+   passReqToCallback: true
+  },
+  function(req, username, password, done){
+   connection.query("SELECT * FROM UserTable WHERE userEmail = ? ", [username],
+   function(err, rows){
+    if(err){
+     console.log(err);
+     return done(err);}
+     
+    if(!rows.length){
+     return done(null, false, req.flash('loginMessage', 'No User Found'));
+    }
+    if(!bcrypt.compareSync(password, rows[0].password))
+     return done(null, false, req.flash('loginMessage', 'Wrong Password'));
+
+    return done(null, rows[0]);
+   });
+  })
+ );
+};
