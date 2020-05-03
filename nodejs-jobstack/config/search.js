d8f3fda7b50b3a185a0bb992203fb75af334d8b6
ChathuriMadhushani <chathuri.2017085@iit.ac.lk>
2020-05-03 06:42:52 +0530

    fewchanges made

--- nodejs-jobstack/config/search.js
+++ nodejs-jobstack/config/search.js
@@ -999,32 +999,3 @@
       }
   }()
 }(window.jQuery);
-function search1(req, res, next){
-
-    var searchTerm = req.query.search;
-    var category = req.query.category;
-
-    let query = "SELECT * FROM company"; 
-    if(searchTerm != '' && category!= ''){
-        query = 'SELECT companyName from company'
-    }
-
-    database.query(query, (err, result) => {
-        if (err){
-
-            req.searchResult = '';
-            req.searchTerm = '';
-            req.category = '';
-            
-            next();
-        }
-
-
-        req.searchResult=result;
-        req.searchTerm= searchTerm;
-        req.category= '';
-
-        next();
-
-    });
-}
