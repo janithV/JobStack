1333da3fe837a5acb528d473d663a6316e558f08
ChathuriMadhushani <chathuri.2017085@iit.ac.lk>
2020-05-04 06:07:45 +0530

    company.model

--- nodejs_backend_jobstack/app/routes/rate.routes.js
+++ nodejs_backend_jobstack/app/routes/rate.routes.js
@@ -1,22 +1,10 @@
 const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/rate.controller");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
+module.exports = (app) => {
 
-//   app.post(
-//     "/api/auth/signup",
-//     [
-//       verifySignUp
-//     ],
-//     controller.signup
-//   );
+  app.post("/api/company/review", controller.rating);
 
-  app.post("/api/auth/rate", controller.rating);
+  app.get("/api/company/reviews", controller.getRating);
+
 };
