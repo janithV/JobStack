5ea6a19b14d95bd454af1e43a20a04c186ba7442
ChathuriMadhushani <chathuri.2017085@iit.ac.lk>
2020-05-04 06:17:41 +0530

    rate.model

--- nodejs_backend_jobstack/app/models/rate.model.js
+++ nodejs_backend_jobstack/app/models/rate.model.js
@@ -1,33 +1,26 @@
 module.exports = (sequelize, Sequelize) => {
-    const Rating = sequelize.define("RatingsTable", {
-      
-            userId: {
-              type: Sequelize.INTEGER,
-              primaryKey: true,
-            },
-            companyId: {
-              type: Sequelize.STRING,
-              primaryKey: true,
-            },
-            dateRated: {
-              type: Sequelize.STRING,
-              primaryKey: true,
-            },
-            rating: {
-              type: Sequelize.DOUBLE
-            },
-            good: {
-              type: Sequelize.TINYINT
-            },
-            average: {
-                type: Sequelize.TINYINT
-            },
-            bad: {
-                type: Sequelize.TINYINT
-            },
-    }, {freezeTableName: true, timestamps: false}
-    );
+  const Rating = sequelize.define("RatingsTable", {
     
-    return Rating;
-  };
+          userId: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+          },
+          companyId: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+          },
+          dateRated: {
+            type: Sequelize.STRING,
+            primaryKey: true,
+          },
+          rating: {
+            type: Sequelize.INTEGER
+          },
+          review: {
+            type: Sequelize.STRING
+          }
+  }, {freezeTableName: true, timestamps: false}
+  );
   
+  return Rating;
+};
